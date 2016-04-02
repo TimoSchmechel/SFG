e5db1da5693d5776d1629521a57bfe2136048304
AVataRR626 <matt.cabanag@gmail.com>
2016-04-02 21:59:17 +1100

    damage and knockback systems (standard unity 3rd person chars)

---
+++ SFG_UnityProject/Assets/SFG/Scripts/SFGFighterController.cs
@@ -0,0 +1,104 @@
+﻿/*
+Script for Simple Fighting Game
+By IGDA Macquarie University
+
+Project Contributors:
+Matt Cabanag
+Garion Knapp
+Joshia Braico
+
+This Script:
+Matt Cabanag
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class SFGFighterController : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    
+    [System.Serializable]
+    public class AxisRestrictor
+    {
+        public bool x;
+        public bool y;
+        public bool z;
+
+    }
+    public AxisRestrictor axisRestrictions;
+
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode jumpKey;
+
+    public ThirdPersonCharacter myController;
+
+    private Vector3 originalPos;
+
+    private bool jumping = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myController = GetComponent<ThirdPersonCharacter>();
+
+        originalPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveDir = Vector3.zero;
+        jumping = false;
+
+        if(Input.GetKey(leftKey))
+        {
+
+            moveDir.x = -moveSpeed * Time.deltaTime;
+            
+        }
+
+        if(Input.GetKey(rightKey))
+        {
+            moveDir.x = moveSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(jumpKey))
+        {
+            jumping = true;
+        }
+
+        myController.Move(moveDir, false, jumping);
+
+        AxisRestrict();
+
+    }
+
+    void AxisRestrict()
+    {
+        Vector3 newPos = transform.position;
+
+        if(axisRestrictions.x)
+        {
+            newPos.x = originalPos.x;
+        }
+
+        if (axisRestrictions.y)
+        {
+            newPos.y = originalPos.y;
+        }
+
+        if (axisRestrictions.z)
+        {
+            newPos.z = originalPos.z;
+        }
+
+        transform.position = newPos;
+    }
+
+}
