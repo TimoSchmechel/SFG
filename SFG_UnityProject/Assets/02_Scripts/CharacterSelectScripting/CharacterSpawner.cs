bff6b05bb637cc3b43fe7e26bb4dbb2780f831b8
AVataRR626 <matt.cabanag@gmail.com>
2016-08-03 14:09:22 +1000

    prep for surgery
    made  2nd characters folder to start work on character selection system

---
+++ SFG_UnityProject/Assets/02_Scripts/CharacterSelectScripting/CharacterSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class SpawnSettings
+    {
+        public int [] characterSelections = new int[4];
+    }
+    public SpawnSettings spawnSettings;
+
+    public GameObject[] CharacterList;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
