85e4a52d78fd1814cc76875a8496be876ea0a0bb
StellarSoup <kolpiou@live.com>
2016-08-17 14:06:11 +1000

    Fixed Up Character Select
    Character Select Screen Updated
    New Controls added for TFGH and IJKL

--- SFG_UnityProject/Assets/02_Scripts/CharacterSelectScripting/ChooseCharacter.cs
+++ SFG_UnityProject/Assets/02_Scripts/CharacterSelectScripting/ChooseCharacter.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 public class ChooseCharacter : MonoBehaviour {
 
     public int charIndex = -1;
@@ -52,46 +53,60 @@ public class ChooseCharacter : MonoBehaviour {
         //Sets what controller will be used
         public void SelectController(string mode)
         {
-            if (mode.Equals("Arrows"))
-            {
-                ArrowMovement();
-            }
-            if (mode.Equals("WASD"))
-            {
-                WASDMovement();
-            }
+		if (mode.Equals ("Arrows")) {
+			ArrowMovement ();
+		} else if (mode.Equals ("WASD")) {
+			WASDMovement ();
+		} else if (mode.Equals ("TFGH")) {
+			TFGHMovement ();
+		} else if (mode.Equals ("IJKL")) {
+			IJKLMovement ();
+		}
         }
+
+
         //Controls the character selection with arrow keys
         private void ArrowMovement()
-        {
-        if (Input.anyKeyDown)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 Left();
-            }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            }else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 Right();
             }
-        }
         }
     //Controls the character selection with WASD keys
     private void WASDMovement()
     {
-        if (Input.anyKeyDown)
-        {
             if (Input.GetKeyDown(KeyCode.A))
             {
                 Left();
-            }
-            if (Input.GetKeyDown(KeyCode.D))
+            }else if (Input.GetKeyDown(KeyCode.D))
             {
                 Right();
             }
-        }
     }
 
+	//Controls the character selection with TFGH keys
+	private void TFGHMovement(){
+		if (Input.GetKeyDown (KeyCode.F)) {
+			Left ();
+		} else if (Input.GetKeyDown(KeyCode.H)) {
+			Right ();
+		}
+	}
+		
+
+	//Controls the character selection with IJKL Keys
+	private void IJKLMovement(){
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Left ();
+		} else if (Input.GetKeyDown (KeyCode.L)) {
+			Right ();
+		}
+	}
+	//Controls camera movement
     private void MoveCamera()
     {
         transform.localPosition = new Vector3(charIndex*2, 0, 0);
---
+++ SFG_UnityProject/Assets/CharacterSelectController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectController : MonoBehaviour {
+
+	//Handles the character select screen
+
+	//Go To Main Menu
+	public void MainMenu(){
+		SceneManager.LoadScene ("MainMenu Canvas");
+	}
+}
