73b8412460372931ce89059d592b124fc181c0a9
thatrandomguy80 <thatrandomguy80@gmail.com>
2016-08-17 15:20:08 +1000

    Map Selection and partial menu linkage
    Added basic map selection screen and linked from main menu to maps
    mostly :smiley:

---
+++ SFG_UnityProject/Assets/02_Scripts/CharacterSelectScripting/MapSelecter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MapSelecter : MonoBehaviour {
+
+    public Sprite[] imgs;
+    public string[] levels = { "Map Forest", "Map Rooftop Alley", "Map Space", "Map Train", "Map Volcano" };
+    private int index = 0;
+
+    private Image imageBoard;
+	// Use this for initialization
+	void Start () {
+        imageBoard = GameObject.Find("Image").GetComponent<Image>();
+        imageBoard.sprite = imgs[index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Left();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Right();
+        }
+    }
+
+    public void confirm()
+    {
+        //load selected level
+        SceneManager.LoadScene(levels[index]);
+    }
+    public void back()
+    {
+        //go back to main menu???
+        SceneManager.LoadScene("MainMenu Canvas");
+    }
+
+    public void Left()
+    {
+        if(index == 0)
+        {
+            index = imgs.Length;
+        }else
+        {
+            index--;
+        }
+        imageBoard.sprite = imgs[index];
+    }
+
+    public void Right()
+    {
+        if (index == imgs.Length)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+        imageBoard.sprite = imgs[index];
+    }
+}
